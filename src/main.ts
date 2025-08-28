299e19edf48b0925590975a06af5916217183402
Cláudio Roberto <claudioroberto0707@outlook.com>
2025-08-28 17:08:32 -0300

    refactor: config cors e main

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import cookieParser from 'cookie-parser'; 
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  app.useGlobalPipes(new ValidationPipe({transform: true}));
+  app.use(cookieParser()); 
+
+  app.enableCors({
+    origin: 'http://localhost:5173', // A URL do seu frontend
+    credentials: true,
+  });
+
+  await app.listen(3000);
 }
 bootstrap();
